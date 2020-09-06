71ece60a8d04855385ba4124b2e1237d9fd6184b
Rahmad Aulia <rahmadaulia40.ra@gmail.com>
2020-09-07 00:36:40 +0700

    design login

---
+++ KeepKas/db/fb.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+   apiKey: "AIzaSyCcf8tjg-TmWbrKg8jJVZp8S3XoAvzaptI",
+   authDomain: "keepkas-f2253.firebaseapp.com",
+   databaseURL: "https://keepkas-f2253.firebaseio.com",
+   projectId: "keepkas-f2253",
+   storageBucket: "keepkas-f2253.appspot.com",
+   messagingSenderId: "559730097970",
+   appId: "1:559730097970:web:7065479f629347c3f3612b",
+   measurementId: "G-EHZH09MKZX"
+ };
+ // Initialize Firebase
+ export default
+ firebase.initializeApp(firebaseConfig);
